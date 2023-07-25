d68977428c03ae3e502b06b65bde54bed9dab171
daiji <jialu.dai@stud.unifi.it>
2023-07-25 21:13:08 +0200

    add implementation of Displayinlist() in UserView
    Signed-off-by: daiji <jialu.dai@stud.unifi.it>

--- UserView.cpp
+++ UserView.cpp
@@ -21,3 +21,12 @@ void UserView::Menu() {
     std::cout<<"5. rinomina la lista"<<std::endl;
     std::cout<<"insesci il numero............."<<std::endl;
 }
+
+
+void UserView::Displayinlist() {
+    std::cout<<"**********************************************"<<std::endl;
+    std::cout<<"1. inserisce un nuovo oggetto"<<std::endl;
+    std::cout<<"2. cancella un oggetto"<<std::endl;
+    std::cout<<"3. torna alla menu: "<<std::endl;
+    std::cout<<"insesci il numero............."<<std::endl;
+}
